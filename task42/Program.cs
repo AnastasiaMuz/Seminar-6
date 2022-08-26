b0b58697ab8767bc1c8fc50e7a65d44588484c41
«Anastasia» <muzalkova.anastasia@gmail.com>
2022-08-26 07:40:19 +0300

    add all

---
+++ task42/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+// const int NUMBER = 2;
+// int number = NUMBER;
+// int size = (int)Math.Log2(number)+1;
+// int[] array = new int[size];
+
+// for(int i = 0; i < array.Length; i++)
+// {
+//     array[i] = number % 2;
+//     number = number / 2;
+// }
+
+
+// void ReverceArray (int[] array)
+// {
+//     for (int i = 0; i <array.Length/2; i++)
+//     {
+//         int temp = array[i];
+//         array[i] = array[array.Length - 1 - i];
+//         array[array.Length - 1 - i] = temp;
+//     }
+// }
+// ReverceArray(array);
+// Console.WriteLine($"Число {NUMBER} в двоичной системе равно {string.Join ("", array)}");
+
+
+Console.WriteLine ("Введите десятичное число: ");
+int number = Convert.ToInt32 (Console.ReadLine ());
+GetBinaryView(number);
+
+void GetBinaryView(int num)
+{
+    if(num == 0) return;
+    GetBinaryView (num / 2);
+    Console.Write(num % 2);
+}
+
---
+++ task45/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+int [] array = {1, 2, 3, 4, 5};
+int [] arr = new int [array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    arr[i] = array[i];
+}
+Console.WriteLine($"Исходный массив {string.Join (", ", array)}");
+Console.WriteLine($"Новый массив {string.Join (", ", arr)}");
